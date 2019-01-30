7d3d5130a4d4450e86fea98e3041fc569924b1bb
kanuahs <kanuahs@gmail.com>
2019-01-30 13:06:25 +0530

    Changed helm and kubectl version to short

--- kubernetes/helm-update.sh
+++ kubernetes/helm-update.sh
@@ -6,4 +6,4 @@ curl https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash
 helm completion bash > helm_autocomplete \
  && sudo mv helm_autocomplete /etc/bash_completion.d/helm
 
-helm version --client
+helm version --short --client
